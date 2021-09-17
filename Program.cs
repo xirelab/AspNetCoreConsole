930c9e1f2859d3ac1d8c51f062ef9041036114f6
shijuloves <shijuloves@gmail.com>
2021-09-17 21:47:57 +1000

    adyam

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System;
+using System.Collections;
+
+namespace firtApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!!");
+            Console.WriteLine("value: "+ IsValid(123).toString());
+        }
+
+        bool IsValid(long number)
+        {
+            return true;
+        }
+    }
+
+    class Student : IEnumerable //<int>
+    {
+        public IEnumerator Student(Stream stream)
+        {
+
+        }
+    }
+}
